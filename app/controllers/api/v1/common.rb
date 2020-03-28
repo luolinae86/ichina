7eb41c272b77a4455955523b5c44b7943caa169f
luolin <luolinae86@gmail.com>
2020-03-28 21:19:50 +0800

    return tips on config

--- app/controllers/api/v1/common.rb
+++ app/controllers/api/v1/common.rb
@@ -20,14 +20,10 @@ module API
         requires :uuid, type: String, desc: 'uuid is needed'
       end
       get '/common/config' do
-        # 当前用户的权限 考虑做缓存
-        config = Rails.cache.fetch(current_user.phone + '_config', expires_in: 1.seconds) do
-          config = {}
-          config[:account_ability] = {
-            has_rider_group: current_user.rider_group_member.blank? ? false : true,
-            is_group_manager: current_user.rider_group.blank? ? false : true
-          }
-        end
+        config = {}
+        config[:system] = {
+          tips: '近期英国疫情增长呈现下降走势'
+        }
         present response: success_response, config: config
       end
     end
