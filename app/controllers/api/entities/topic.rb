2bbd0aee565b1e116e47924548bd2d0ffab3572f
luolin <luolinae86@gmail.com>
2020-04-03 14:56:14 +0800

    return topic id

--- app/controllers/api/entities/topic.rb
+++ app/controllers/api/entities/topic.rb
@@ -3,7 +3,7 @@
 module API
   module Entities
     class Topic < Grape::Entity
-      expose :content, :latitude, :longitude, :status, :topic_type, :viewed_count, :uuid, :created_at, :is_urgent
+      expose :id, :content, :latitude, :longitude, :status, :topic_type, :viewed_count, :uuid, :created_at, :is_urgent
       expose :customer, using: API::Entities::Customer
       expose :complaints, using: API::Entities::Complaint
     end
